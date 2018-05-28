39755f2e5a0647f4816a446cf07cf2509022838a
Liu HanYu <302800746@qq.com>
2018-05-28 15:50:26 +0800

    blogproject with Django

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Tag,Category,Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=['title','created_time','modified_time','category','author']
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Category)
